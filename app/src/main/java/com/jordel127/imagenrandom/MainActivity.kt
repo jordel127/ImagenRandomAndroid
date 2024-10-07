8cadfd91c461d8a59cb255652b203744fd8c127f
jordel127 <joel.perez@institutvidreres.cat>
2024-10-07 13:41:02 +0200

    DadoFuncional

--- app/src/main/java/com/jordel127/imagenrandom/MainActivity.kt
+++ app/src/main/java/com/jordel127/imagenrandom/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.jordel127.imagenrandom
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imagenDado: ImageView = findViewById(R.id.imageViewDado)
+        val bottonRoll: Button = findViewById(R.id.buttonRoll)
+
+
+        bottonRoll.setOnClickListener{
+            val numDado = Random.nextInt(0,7)
+            when (numDado) {
+                1 -> imagenDado.setImageResource(R.drawable.dice_1)
+                2 -> imagenDado.setImageResource(R.drawable.dice_2)
+                3 -> imagenDado.setImageResource(R.drawable.dice_3)
+                4 -> imagenDado.setImageResource(R.drawable.dice_4)
+                5 -> imagenDado.setImageResource(R.drawable.dice_5)
+                else -> imagenDado.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 }
